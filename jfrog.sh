b0ea3313b0cba5558b48da4b16c2bbfbcf92a316
Ubuntu <ubuntu@ip-172-31-47-118.ap-south-1.compute.internal>
2020-01-17 13:41:42 +0000

    code

---
+++ jfrog.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "start the build"
+
+mvn package
+
+status=$?
+
+if [ $status == 0 ]
+
+  then
+  	echo "start ci build"
+    
+    project=wezva;
+    branch=$(git  git rev-parse --abbrev-ref HEAD)
+    buildtype=$JOB_NAME
+    buildNo=$BUILD_NUMBER
+  	tar -cvfz $project_$branch_$buildtype_$buildNo_.zip ./webapp/target/*.war
+
+  	curl -u 'admin:38920139' -XPUT "http://13.233.99.57:8081/artifactory/CI-BUILDS/" -T $project_$branch_$buildtype_$buildNo_.zip
+
+  	rm -rf ./target
+
+else
+	echo "this is artifactory niku ardhamavthundha"
+fi
