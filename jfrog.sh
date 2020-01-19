7f99cd3d5d9e9828aae136271d2e7c48b83bd811
Ubuntu <ubuntu@ip-172-31-32-219.ap-south-1.compute.internal>
2020-01-19 14:09:08 +0000

    aaaasd

--- jfrog.sh
+++ jfrog.sh
@@ -10,15 +10,15 @@ if [ $status == 0 ]
 
   then
   	echo "start ci build"
-    
+    branch=$GiT_BRANCH 
     project=wezva;
     buildtype=$JOB_NAME
     buildNo=$BUILD_NUMBER
-  	tar -cvf $project-$buildtype-$buildNo.zip ./webapp/target/*.war
+  	tar -cvf $project-$branch-$buildtype-$buildNo.zip ./webapp/target/*.war
 
-  	curl -u 'admin:38920139' -XPUT "http://35.154.161.74:8081/artifactory/CI-BUILDS/" -T $project-$buildtype-$buildNo.zip
+  	curl -u 'admin:38920139' -XPUT "http://35.154.161.74:8081/artifactory/CI-BUILDS/" -T $project-$branch-$buildtype-$buildNo.zip
 
-  	rm -rf ./$project-$buildtype-$buildNo.zip
+  	rm -rf ./$project-$branch-$buildtype-$buildNo.zip
 
   else
     	echo "unable to push arifact bcz of build failure"
